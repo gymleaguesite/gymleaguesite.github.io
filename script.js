bfc2c84e86e7928fc7e9d83d17433c873c730150
gym league <gymleaguesite@gmail.com>
2024-07-12 21:03:37 +0200

    Final

--- script.js
+++ script.js
@@ -14,22 +14,33 @@ const powerUps = [
     { name: 'Small Pump Potion', duration: 15, cost: 15000 },
     { name: 'Secret Beans', duration: 10, cost: 18750 },
     { name: 'Angel potion', duration: 20, cost: 75000 },
-    { name: 'Small Speed Potion', duration: 15, cost: 15000 } // New entry for Small Speed Potion
+    { name: 'Small Speed Potion', duration: 15, cost: 15000 }
 ];
+
+document.getElementById('selectAll').addEventListener('change', function() {
+    const checkboxes = document.querySelectorAll('#timeForm input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = this.checked;
+    });
+});
 
 function calculateByTime() {
     const trainingTime = parseFloat(document.getElementById('trainingTime').value) * 60; // Convert to minutes
-    let results = '<h3>Result for time training:</h3><table border="1"><tr><th>Name</th><th>Amount</th><th>Price ($)</th></tr>';
+    const selectedPowerUps = Array.from(document.querySelectorAll('#timeForm input[type="checkbox"]:checked')).map(input => input.value);
+
+    let results = '<h3>Result for selected power-ups and training time:</h3><div class="table-container"><table border="1"><tr><th>Name</th><th>Amount</th><th>Price ($)</th></tr>';
     let totalCost = 0;
 
     powerUps.forEach(powerUp => {
-        const quantity = Math.ceil(trainingTime / powerUp.duration);
-        const cost = quantity * powerUp.cost;
-        totalCost += cost;
+        if (selectedPowerUps.includes(powerUp.name)) {
+            const quantity = Math.ceil(trainingTime / powerUp.duration);
+            const cost = quantity * powerUp.cost;
+            totalCost += cost;
 
-        results += `<tr><td>${powerUp.name}</td><td>${quantity.toLocaleString()}</td><td>${cost.toLocaleString()}</td></tr>`;
+            results += `<tr><td>${powerUp.name}</td><td>${quantity.toLocaleString()}</td><td>${cost.toLocaleString()}</td></tr>`;
+        }
     });
 
-    results += `<tr><td colspan="2">Total cost</td><td>${totalCost.toLocaleString()}</td></tr></table>`;
+    results += `<tr><td colspan="2">Total cost</td><td>${totalCost.toLocaleString()}</td></tr></table></div>`;
     document.getElementById('timeResults').innerHTML = results;
 }
